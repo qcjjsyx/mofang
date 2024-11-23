96d194f8560f005c4a6d7bc3a1d556671a70920c
黄渊 <1697242501@qq.com>
2024-11-23 16:12:48 +0800

    opencv配置完成

--- mofang/utility.hpp
+++ mofang/utility.hpp
@@ -6,8 +6,43 @@
 
 //wiatKey() 用来暂停的功能
 
+#include<opencv2/opencv.hpp>
+#include<string>
+using namespace cv;
+using namespace std;
+
+class Utility
+{
 
 
 
+public:
+
+	/*从图像中获取魔方状态*/
+	string readFromImage(string pathB, string pathL, string pathU, string pathR, string pathF, string pathD) {
+		Mat B = imread(pathB);
+		Mat L = imread(pathL);
+		Mat U = imread(pathU);
+		Mat R = imread(pathR);
+		Mat F = imread(pathF);
+		Mat D = imread(pathD);
+
+		string ret = "";
+		ret += image2Str(B);
+		ret += image2Str(L);
+		ret += image2Str(U);
+		ret += image2Str(R);
+		ret += image2Str(F);
+		ret += image2Str(D);
+
+		return ret;
+	}
+
+private:
+	string image2Str(Mat img) {
+
+	}
+
+};
 
 #endif
---
+++ test.cpp
@@ -0,0 +1,11 @@
+class Utility
+{
+
+	
+
+public:
+	Mat B, L, U, R, F, D;
+	Utility();
+	~Utility();
+	
+};
